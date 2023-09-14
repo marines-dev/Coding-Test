fb34b6da33a94de23d747d08f354aaa0cc97fa48
marines-dev <130581625+marines-dev@users.noreply.github.com>
2023-09-14 15:09:28 +0900

    [level 2] Title: 카펫, Time: 0.20 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int area = brown + yellow;
+        
+        for(int i = (int)Math.Sqrt(area); i <= area; i++)
+        {
+            if(area % i == 0)
+            {
+                answer[0] = Math.Max(area / i, i);
+                answer[1] = Math.Min(area / i, i);
+                if((answer[0] - 2) * (answer[1] - 2) == yellow)
+                    break;
+            }
+        }
+        
+        return answer;
+    }
+}
